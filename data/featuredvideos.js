db01679fbaab3417b05a9ce0e716cea1113873eb
MOB-EXTRA <marcjohnfordc@gmail.com>
2026-07-13 14:06:08 +0800

    Updated files via phone

--- data/featuredvideos.js
+++ data/featuredvideos.js
@@ -8,6 +8,17 @@ const featuredVideos = [
     {
         title: "How to Redeem Code in CODM",
         url: "https://youtu.be/p8ahCxRSIWo?si=z5IKba-HSBxG3bSY"
-    }
+    },
+    {
+        title: "Space Character in you CODM IGN?",
+        url: "https://youtu.be/d1B8YC1gY2A?si=IMRXKc3CfMDPjTmv"
+    },
+
+    {
+        title: "Match Replay in Call of Duty: Mobile",
+        url: "https://youtu.be/RBKyOLs2wlE?si=CayWA4b_PjhoZJ3E"
+    },
+
+
 
 ];
